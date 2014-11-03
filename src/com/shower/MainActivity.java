0ae39f255aa0888071af19be03e9aa4ad538f15b
KaluYu <741470894@qq.com>
2014-11-04 00:17:37 +0800

    test

--- src/com/shower/MainActivity.java
+++ src/com/shower/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 	}
-	
+	//https://github.com/kalusyu/fontxiuxiu.git
 	// 1024 * 600
 }
